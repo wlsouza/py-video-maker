1f1c8cb1ca120fdbcb6fd5bdcb5cd39e33a9795f
Wellington Lorenço <wlsouza@protonmail.com>
2020-02-16 16:09:42 -0300

    feat(interface): add search prefix input

--- src/interface/ui.py
+++ src/interface/ui.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import os
+
 
 class Ui:
 
@@ -8,19 +10,43 @@ class Ui:
         self._search_data = {}
 
     def run(self):
-        self.ask()
+        self.ask_search_term()
+        self.ask_search_prefix()
 
-    def ask(self):
-        response = input(f'Type a Wikipedia search term of \'G\' to fetch google trends terms: ')
+    def ask_search_term(self):
+        self.clear_screen()
+        response = input(f'Type a Wikipedia search term of \'G\' to fetch google trends terms: ').strip()
         if response:
             if response.upper() == 'G':
                 print(f'I\'m sorry, this function is not yet implemented.')
                 # To Do: implement the search google trends method.
             else:
                 self._search_data['search_term'] = response
-                # print(self._search_data['search_term'])
         else:
-            self.ask()
+            self.ask_search_term()
+
+    def ask_search_prefix(self):
+        self.clear_screen()
+        options = {'1': 'Who is', '2': 'What is', '3': 'The history of', '0': 'CANCEL'}
+        print(f'The search term is: {self._search_data.get("search_term")}')
+        print(f'You must inform the prefix for your video:\n')
+        for key, value in options.items():
+            print(f'[{key}] - {value}')
+        response = input(f'\nChoose one option {list(options.keys())}: ').strip()
+        if response:
+            if response in options:
+                self._search_data['search_prefix'] = response
+            else:
+                self.ask_search_prefix()
+        else:
+            self.ask_search_prefix()
+
+    @staticmethod
+    def clear_screen():
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
 
 
 if __name__ == '__main__':
