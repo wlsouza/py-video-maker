e7c19826cc1e80a7415636912517226eb13b103b
Wellington Lorenço <wlsouza@protonmail.com>
2020-02-17 16:31:10 -0300

    feat(domain): add regex to remove date from summary

--- src/domain/text_robot.py
+++ src/domain/text_robot.py
@@ -2,26 +2,32 @@
 # -*- coding: UTF-8 -*-
 
 import nltk
+import re
 from src.infrastructure.wikipedia_service import WikipidiaService
 
 
-class TextRobot():
+class TextRobot:
 
     def __init__(self, search_term=None, search_prefix=None):
         self.search_term = search_term
         self.original_page = None
-        self.treated_text = None
+        self.treated_summary = None
+        self.summary_sentences = []
 
     def run(self):
-        self.fetch_content_from_wikipedia()
-        self.split_contents_to_sentences()
+        self.fetch_page_from_wikipedia()
+        self.sanitize_content()
+        self.split_contents_into_sentences()
 
-    def fetch_content_from_wikipedia(self):
+    def fetch_page_from_wikipedia(self):
         try:
             self.original_page = WikipidiaService.get_page(title=self.search_term, prefix_lang='pt')
         except Exception as error:
             raise Exception(f' Error while fetching text from wikipedia -> {error}')
 
-    def split_contents_to_sentences(self):
-        summary = self.original_page.summary
-        self.treated_text = nltk.sent_tokenize(summary)
+    def sanitize_content(self):
+        self.treated_summary = re.sub(r'\((?:\([^()]*\)|[^()])*\)', '', self.original_page.summary)
+
+    def split_contents_into_sentences(self):
+        summary = self.treated_summary
+        self.summary_sentences = nltk.sent_tokenize(summary)
