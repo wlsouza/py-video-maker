f151822bf0305d08883e9d4acc86e41b1b4536df
Wellington Lorenço <wlsouza@protonmail.com>
2020-02-27 19:34:27 -0300

    feat(domain): add trasition effects at video

--- src/config.py
+++ src/config.py
@@ -14,39 +14,59 @@ image_robot = {}
 
 # Video Robot #
 video_robot = {
-    0: {
-        'size': (1920, 400),  # Size of image
-        'align': 'South',  # Position of text at image
-        'position': ('center', 'bottom')  # Position of image at video
+    'text_clip': {
+        0: {
+            'size': (1920, 400),  # Size of image
+            'align': 'South',  # Position of text at image
+            'position': ('center', 'bottom'),  # Position of image at video
+            'start': 2,  # Seconds to start text_clip
+            'duration': 5  # Duration of text_clip
+        },
+        1: {
+            'size': (1920, 1080),
+            'align': 'South',
+            'position': ('center', 'bottom'),
+            'start': 2,
+            'duration': 5
+        },
+        2: {
+            'size': (800, 1080),
+            'align': 'West',
+            'position': ('left', 'center'),
+            'start': 2,
+            'duration': 5
+        },
+        3: {
+            'size': (1920, 400),
+            'align': 'South',
+            'position': ('center', 'bottom'),
+            'start': 2,
+            'duration': 5
+        },
+        4: {
+            'size': (1920, 1080),
+            'align': 'South',
+            'position': ('center', 'bottom'),
+            'start': 2,
+            'duration': 5
+        },
+        5: {
+            'size': (800, 1080),
+            'align': 'West',
+            'position': ('left', 'center'),
+            'start': 2,
+            'duration': 5
+        },
+        6: {
+            'size': (1920, 400),
+            'align': 'South',
+            'position': ('center', 'bottom'),
+            'start': 2,
+            'duration': 5
+        }
     },
-    1: {
+    'image_clip': {
         'size': (1920, 1080),
-        'align': 'South',
-        'position': ('center', 'bottom')
-    },
-    2: {
-        'size': (800, 1080),
-        'align': 'West',
-        'position': ('left', 'center')
-    },
-    3: {
-        'size': (1920, 400),
-        'align': 'South',
-        'position': ('center', 'bottom')
-    },
-    4: {
-        'size': (1920, 1080),
-        'align': 'South',
-        'position': ('center', 'bottom')
-    },
-    5: {
-        'size': (800, 1080),
-        'align': 'West',
-        'position': ('left', 'center')
-    },
-    6: {
-        'size': (1920, 400),
-        'align': 'South',
-        'position': ('center', 'bottom')
+        'duration': 8  # Duration of image_clip
     }
 }
--- src/domain/video_robot.py
+++ src/domain/video_robot.py
@@ -17,24 +17,36 @@ class VideoRobot:
     def compose_video(self):
         images_list = []
         clips_list = []
+
         for sentence_index, sentence in enumerate(self.video.sentences):
             image = sentence.treated_image
+
             # Getting configs
-            align = config[sentence_index].get('align')
-            position = config[sentence_index].get('position')
-            size = config[sentence_index].get('size')
+            text_clip_align = config['text_clip'][sentence_index].get('align')
+            text_clip_position = config['text_clip'][sentence_index].get('position')
+            text_clip_size = config['text_clip'][sentence_index].get('size')
+            text_clip_start = config['text_clip'][sentence_index].get('start')
+            text_clip_duration = config['text_clip'][sentence_index].get('duration')
+            image_clip_duration = config['image_clip'].get('duration')
+            image_clip_size = config['image_clip'].get('size')
 
             # Create text clip
-            text_clip = TextClip(sentence.text, color='white', font="Amiri-Bold", size=size, fontsize=65, method='caption', align=align).set_duration(5).set_start(3).set_position(position).crossfadein(2)
+            text_clip = TextClip(sentence.text, color='white', font="Amiri-Bold", size=text_clip_size, fontsize=65, method='caption', align=text_clip_align)
+            text_clip = text_clip.set_duration(text_clip_duration).set_start(text_clip_start).set_position(text_clip_position).crossfadein(2)  # Apply settings and effects
 
-            # # Creating image clip
-            image_clip = ImageClip(image.full_path).set_duration(8)
+            # Create image clip
+            image_clip = ImageClip(image.full_path).set_duration(4)
+            image_clip_fx = image_clip.fx(vfx.colorx, 0.4).crossfadein(1).set_duration(image_clip_duration).set_position(('center', 'center')).resize(lambda t: 1 + 0.1 * (t-1.5) if t > 1.5 else 1)
 
-            # # Overlay text on image clip
-            sentence_clip = CompositeVideoClip([image_clip, text_clip])
+            sentence_clip = concatenate_videoclips([image_clip, image_clip_fx], padding=-1,  method="compose")
 
+            # Overlay text on image clip
+            sentence_clip = CompositeVideoClip([sentence_clip, text_clip])
+            sentence_clip = sentence_clip.crossfadein(1)  # Apply settings and effects
+
+            # Append result in a list of clips
             clips_list.append(sentence_clip)
 
         # Concatenate video clips
-        concat_clip = concatenate_videoclips(clips_list, method="compose")
-        concat_clip.write_videofile("test.mp4", fps=24)
+        concat_clip = concatenate_videoclips(clips_list, padding=-1,  method="compose")  # padding=-N is necessary due to the crossfade(N) effect
+        concat_clip.write_videofile("test.mp4", fps=15)
